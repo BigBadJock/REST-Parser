47ee55ebf1b6f94e966c8852def94ae059514445
John McArthur <john.mcarthur@silvercode.co.uk>
2019-11-28 23:03:48 +0000

    Added Contains to string

--- REST-Parser/REST-Parser/ExpressionGenerators/StringExpressionGenerator.cs
+++ REST-Parser/REST-Parser/ExpressionGenerators/StringExpressionGenerator.cs
@@ -2,6 +2,7 @@
 using REST_Parser.ExpressionGenerators.Interfaces;
 using System;
 using System.Linq.Expressions;
+using System.Reflection;
 
 namespace REST_Parser.ExpressionGenerators
 {
@@ -11,6 +12,7 @@ namespace REST_Parser.ExpressionGenerators
         {
             try
             {
+
 
                 switch (restOperator)
                 {
@@ -22,6 +24,8 @@ namespace REST_Parser.ExpressionGenerators
                         return Expression.Lambda<Func<T, bool>>(
                             Expression.NotEqual(Expression.PropertyOrField(parameter, field), Expression.Constant(value)),
                             parameter);
+                    case "contains":
+                        return this.GetContainsExpression(parameter, field, value);
                     default:
                         throw new REST_InvalidOperatorException(field, restOperator);
                 }
@@ -34,6 +38,16 @@ namespace REST_Parser.ExpressionGenerators
             {
                 throw new REST_InvalidFieldnameException(string.Format("field={0} value={1}", field, value));
             }
+        }
+
+        private Expression<Func<T, bool>> GetContainsExpression(ParameterExpression parameter, string field, string value)
+        {
+            Expression property = Expression.Property(parameter, field);
+            ConstantExpression search = Expression.Constant(value, typeof(string));
+            MethodInfo method = typeof(string).GetMethod("Contains", new[] { typeof(string) });
+            var containsMethodExp = Expression.Call(property, method, search);
+            return Expression.Lambda<Func<T, bool>>(containsMethodExp, parameter);
+
         }
     }
 }
---
+++ REST-Parser/RestParserTests/RestToLinq_Contains_Tests.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using REST_Parser;
+using REST_Parser.ExpressionGenerators;
+using REST_Parser.ExpressionGenerators.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestParserTests
+{
+
+    [TestClass]
+    public class RestToLinq_Contains_Tests
+    {
+        IQueryable<TestItem> data;
+        private IStringExpressionGenerator<TestItem> stringExpressionGenerator;
+        private IIntExpressionGenerator<TestItem> intExpressionGenerator;
+        private IDateExpressionGenerator<TestItem> dateExpressionGenerator;
+        private IDoubleExpressionGenerator<TestItem> doubleExpressionGenerator;
+        private IDecimalExpressionGenerator<TestItem> decimalExpressionGenerator;
+        private IBooleanExpressionGenerator<TestItem> booleanExpressionGenerator;
+        RestToLinqParser<TestItem> parser;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            List<TestItem> d1 = new List<TestItem>();
+            d1.Add(new TestItem { Id = 1, FirstName = "Bob", Surname = "Roberts", Amount = 4, Price = 4, Rate = 2.1m, Birthday = Convert.ToDateTime("1966/01/01"), Flag = true });
+            d1.Add(new TestItem { Id = 2, FirstName = "John", Surname = "McArthur", Amount = 5, Price = 5.25, Rate = 2.2m, Birthday = Convert.ToDateTime("1968/01/01"), Flag = false });
+            d1.Add(new TestItem { Id = 3, FirstName = "James", Surname = "McArthur", Amount = 6, Price = 5.5, Rate = 2.3m, Birthday = Convert.ToDateTime("1970/01/01"), Flag = true });
+            d1.Add(new TestItem { Id = 4, FirstName = "James", Surname = "Smith", Amount = 7, Price = 6.5, Rate = 2.4m, Birthday = Convert.ToDateTime("1972/01/01"), Flag = true });
+            this.data = d1.AsQueryable();
+
+            this.stringExpressionGenerator = new StringExpressionGenerator<TestItem>();
+            this.intExpressionGenerator = new IntExpressionGenerator<TestItem>();
+            this.dateExpressionGenerator = new DateExpressionGenerator<TestItem>();
+            this.doubleExpressionGenerator = new DoubleExpressionGenerator<TestItem>();
+            this.decimalExpressionGenerator = new DecimalExpressionGenerator<TestItem>();
+            this.booleanExpressionGenerator = new BooleanExpressionGenerator<TestItem>();
+            this.parser = new RestToLinqParser<TestItem>(stringExpressionGenerator, intExpressionGenerator, dateExpressionGenerator, doubleExpressionGenerator, decimalExpressionGenerator, booleanExpressionGenerator);
+        }
+
+        [DataTestMethod]
+        [DataRow("surname[contains]=Rob", "Roberts")]
+        [DataRow("surname[contains]=mith", "Smith")]
+        public void Contains_String_Test(string rest, string surname)
+        {
+            // arrange
+            List<Expression<Func<TestItem, bool>>> expected = new List<Expression<Func<TestItem, bool>>>();
+            expected.Add(p => p.Surname.Contains(surname));
+
+            // act
+            List<Expression<Func<TestItem, bool>>> expressions = parser.Parse(rest);
+
+            IQueryable<TestItem> selectedData = data;
+            expressions.ForEach(delegate (Expression<Func<TestItem, bool>> where) {
+                selectedData = selectedData.Where(where);
+            });
+            TestItem first = selectedData.FirstOrDefault();
+            // assert
+            Assert.AreEqual(expected.Count, expressions.Count);
+            Assert.AreEqual(1, selectedData.Count());
+
+            Assert.AreEqual(surname, first.Surname);
+        }
+
+    }
+}
