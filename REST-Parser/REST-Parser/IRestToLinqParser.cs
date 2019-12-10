6a7a100c378c329de402aa1ccb47aea29a039fb4
John McArthur <john.mcarthur@silvercode.co.uk>
2019-12-10 22:29:54 +0000

    Updated interface name

---
+++ REST-Parser/REST-Parser/IRestToLinqParser.cs
@@ -0,0 +1,12 @@
+﻿using REST_Parser.Models;
+using System.Linq;
+
+namespace REST_Parser
+{
+    public interface IRestToLinqParser<T>
+    {
+        RestResult<T> Parse(string request);
+
+        RestResult<T> Run(IQueryable<T> source, string rest);
+    }
+}
