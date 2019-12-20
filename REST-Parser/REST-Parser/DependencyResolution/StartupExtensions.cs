2efcbfb6438100cd9090fd760bc17a7df0cd8962
John McArthur <john.mcarthur@silvercode.co.uk>
2019-12-20 00:54:27 +0000

    Added Dependency Injection

---
+++ REST-Parser/REST-Parser/DependencyResolution/StartupExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using REST_Parser.ExpressionGenerators;
+using REST_Parser.ExpressionGenerators.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REST_Parser.DependencyResolution
+{
+    public static class StartupExtensions
+    {
+        public static void RegisterRestParser<T>(this IServiceCollection services)
+        {
+            services.AddSingleton<IBooleanExpressionGenerator<T>, BooleanExpressionGenerator<T>>();
+            services.AddSingleton<IDateExpressionGenerator<T>, DateExpressionGenerator<T>>();
+            services.AddSingleton<IDecimalExpressionGenerator<T>, DecimalExpressionGenerator<T>>();
+            services.AddSingleton<IDoubleExpressionGenerator<T>, DoubleExpressionGenerator<T>>();
+            services.AddSingleton<IIntExpressionGenerator<T>, IntExpressionGenerator<T>>();
+            services.AddSingleton<IStringExpressionGenerator<T>, StringExpressionGenerator<T>>();
+            services.AddSingleton<IRestToLinqParser<T>, RestToLinqParser<T>>();
+        }
+    }
+}
+
